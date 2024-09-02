f9e74677639b418743f185a044b257159c742d3f
Jeegar <jshah@cj.com>
2024-09-02 13:43:47 -0700

    delete

--- ZSH_CUSTOM/install.sh
+++
@@ -1,2 +0,0 @@
-ln -sfv ~/projects/git/jshah/.dotfiles/zsh/jshah.zsh-theme ~/.oh-my-zsh/custom/themes/
-ln -sfv ~/projects/git/jshah/.dotfiles/zsh/jshah.plugin.zsh ~/.oh-my-zsh/custom/plugins/jshah/
