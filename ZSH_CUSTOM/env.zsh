89e2e69849aeaab545a2c009b6476d2f13ce56fb
Jeegar Shah <jshah@cj.com>
2025-02-05 15:52:03 -0800

    changes after new mac m4

--- ZSH_CUSTOM/env.zsh
+++ ZSH_CUSTOM/env.zsh
@@ -2,7 +2,7 @@
 export PROJECTS=$HOME/projects
 
 #DEVDB
-export set BUILD_USER=spud
+export BUILD_USER=spud
 # DB
 export TNS_ADMIN=$HOME
 export DEVDB_HOST=devdb.db.cj.com
@@ -11,8 +11,9 @@ export DEVDB_SID=DEVDB
 
 # Software Setup
 export CJ_GIT_HOME=$PROJECTS/git/cj
-export CJ_SOFTWARE_HOME=$CJ_GIT_HOME/software
+export CJ_SOFTWARE_HOME=$CJ_GIT_HOME/cjdev/software
+export RESIN_HOME=$CJ_SOFTWARE_HOME/resin/resin-4.0.66
 
 # MAVEN
-export MAVEN_OPTS="-Xmx4086m -Xss512m -XX:ReservedCodeCacheSize=256m"
+export MAVEN_OPTS="-Xmx8172m -Xss512m -XX:ReservedCodeCacheSize=256m"
 
